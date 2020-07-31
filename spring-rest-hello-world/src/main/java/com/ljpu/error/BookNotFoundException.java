697e65b74a2a68e563cda08fa8ff261b032b4bf2
Leo Pu <leoljpu@gmail.com>
2020-07-31 10:58:57 -0400

    initial commitment with spring-rest-hello-world

---
+++ spring-rest-hello-world/src/main/java/com/ljpu/error/BookNotFoundException.java
@@ -0,0 +1,9 @@
+package com.ljpu.error;
+
+public class BookNotFoundException extends RuntimeException {
+
+    public BookNotFoundException(Long id) {
+        super("Book id not found : " + id);
+    }
+
+}
