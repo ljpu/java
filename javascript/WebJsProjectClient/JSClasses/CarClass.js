f4e0be38da7779afaf407fd2a7da7af12bb1c8b5
ljpu <ljpu@gmail.com>
2022-01-07 16:24:06 -0500

    javascript/ directory added

---
+++ javascript/WebJsProjectClient/JSClasses/CarClass.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+class Car {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+  }
+  age(x) {
+    return x - this.year;
+  }
+}
+
+/* It could be moved to another <script> and </script> section in CarClassEmbedded.html
+let date = new Date();
+let year = date.getFullYear();
+
+let myCar = new Car("Ford", 2014);
+document.getElementById("demo").innerHTML=
+"My car is " + myCar.age(year) + " years old.";
+*/
---
+++ javascript/WebJsProjectClient/JSClasses/MyJS.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+
+function add(a,b){
+	return a + b;
+}
+
+// The following is for Node.js only
+let a=5, b =187;
+console.log(a,b);
+console.log(add(a,b));
