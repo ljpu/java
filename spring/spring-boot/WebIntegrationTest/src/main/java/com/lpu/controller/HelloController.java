330c0dde9881b54ef6863a2b6684c9ce1be2cd0f
Truth is True <25091011+ljpu@users.noreply.github.com>
2024-08-17 11:40:07 -0400

    Simple Integration test for web

---
+++ spring/spring-boot/WebIntegrationTest/src/main/java/com/lpu/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.lpu.demo.controller;
+
+import com.lpu.demo.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @Autowired
+    private HelloService helloService;
+
+    @GetMapping
+    public String sayHello() {
+        return helloService.getGreeting();
+    }
+}
---
+++ spring/spring-boot/WebIntegrationTest/src/test/java/com/lpu/demo/HelloControllerIntegrationTest.java
@@ -0,0 +1,27 @@
+package com.lpu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HelloControllerIntegrationTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void sayHello_ShouldReturnDefaultMessage() {
+        // Act
+        ResponseEntity<String> response = restTemplate.getForEntity("/hello", String.class);
+
+        // Assert
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualTo("Hello, World!");
+    }
+}
