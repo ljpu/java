13e11401e4ef4e95e73a1cd86c4ef884dc1e3eec
Leo Pu <leopu2nd@gmail.com>
2024-08-19 16:21:25 -0400

    initial commit

---
+++ spring-boot-rest-services-with-unit-and-integration-tests/src/main/java/com/lpu/springboot/model/Course.java
@@ -0,0 +1,12 @@
+package com.lpu.springboot.model;
+
+import java.util.List;
+
+public record Course(String id,
+					 String name,
+					 String description,
+					 List<String> steps) {
+	public void setId(String id) {
+	}
+
+}
