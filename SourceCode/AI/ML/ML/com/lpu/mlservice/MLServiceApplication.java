c1e85092cfa9d4c456ee164ec4df76712713ffe9
Truth is True <25091011+ljpu@users.noreply.github.com>
2024-09-23 11:52:27 -0400

    Upload AI/ML folder

---
+++ SourceCode/AI/ML/ML/com/lpu/mlservice/MLServiceApplication.java
@@ -0,0 +1,11 @@
+package com.lpu.mlservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MLServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MLServiceApplication.class, args);
+    }
+}
